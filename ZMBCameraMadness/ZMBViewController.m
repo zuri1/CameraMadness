16749f0465b7cc202a0186afd626e85c5c0d0015
zuri1 <zuri.biringer@gmail.com>
2013-11-05 13:24:39 -0800

    Add camera functionality

--- ZMBCameraMadness/ZMBViewController.m
+++ ZMBCameraMadness/ZMBViewController.m
@@ -8,7 +8,7 @@
 
 #import "ZMBViewController.h"
 
-@interface ZMBViewController ()
+@interface ZMBViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
@@ -24,6 +24,78 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UIImagePickerController
+
+-(IBAction)choosePhoto:(id)sender
+{
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    [picker setDelegate:self];
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        [picker setSourceType:UIImagePickerControllerSourceTypeCamera];
+    } else {
+        [picker setSourceType:UIImagePickerControllerSourceTypeSavedPhotosAlbum];
+    }
+    
+    [picker setAllowsEditing:YES];
+    [self presentViewController:picker animated:YES completion:^{
+        NSLog(@"Showing Camera");
+    }];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [self dismissViewControllerAnimated:YES completion:^{
+        UIImage *pickedImage = [info objectForKey:UIImagePickerControllerEditedImage];
+        
+        [self applyFilterToImage:pickedImage];
+        
+    }];
+}
+
+- (void)applyFilterToImage:(UIImage *)image
+{
+    // filter the imagae
+    CIContext *context = [CIContext contextWithOptions:nil];
+    
+    CIImage *ciImage = [CIImage imageWithCGImage:image.CGImage];
+    
+    CIFilter *filter = [CIFilter filterWithName:@"CIPhotoEffectInstant"];
+    
+    [filter setValue:ciImage forKey:kCIInputImageKey];
+    
+    CIImage *result = [filter valueForKey:kCIOutputImageKey];
+    
+    CGRect extent = [result extent];
+    
+    CGImageRef cgImage = [context createCGImage:result fromRect:extent];
+    
+    UIImage *filteredImage = [UIImage imageWithCGImage:cgImage];
+    
+    // show the image to the user
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
+    [imageView setImage:filteredImage];
+    [self.view addSubview:imageView];
+    
+    // save the image to the photos album
+    UIImageWriteToSavedPhotosAlbum(filteredImage, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    NSLog(@"User cancelled image selection");
+}
+
+- (void)image: (UIImage *)image didFinishSavingWithError: (NSError *) error
+  contextInfo: (void *) contextInfo
+{
+    if (error) {
+        NSLog(@"Unable to save photo to camera roll");
+    } else {
+        NSLog(@"Saved image to camera roll");
+    }
 }
 
 @end
